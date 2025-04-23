a35972f13577b63cf6e1025a7c6518b93fe375a9
ciyek <aripurnomo606@gmail.com>
2025-04-23 16:31:37 +0700

    feat: extend User model and UserRequest schema to include Phone, Address, and Avatar fields

--- schemas/userSchema.go
+++ schemas/userSchema.go
@@ -1,11 +1,14 @@
 package schemas
 
 type UserRequest struct {
-	FirstName string `json:"firstName" binding:"required"`
-	LastName  string `json:"lastName" binding:"required"`
-	Email     string `json:"email" binding:"required,email"`
-	Password  string `json:"password" binding:"required,min=6"`
-	RoleID    uint   `json:"role_id" binding:"required"`
+	FirstName string  `json:"firstName" binding:"required"`
+	LastName  string  `json:"lastName" binding:"required"`
+	Email     string  `json:"email" binding:"required,email"`
+	Password  string  `json:"password" binding:"required,min=6"`
+	Phone     *string `json:"phone,omitempty"`
+	Address   *string `json:"address,omitempty" `
+	Avatar    *string `json:"avatar,omitempty" `
+	RoleID    uint    `json:"role_id" binding:"required"`
 }
 
 type SignInRequest struct {
